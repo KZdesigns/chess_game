bdc7be3735545f8c9dbfdfa4b4978d1991419af5
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-07-18 11:50:00 -0700

    board complete

--- pieces.rb
+++ pieces.rb
@@ -18,16 +18,18 @@ class Piece
     end
 
     def pos=(val)
-
+        board[pos] = val
     end
     
     def Symbol
-
+        raise NotImplementedError
     end
 
     private
 
     def move_into_check?(end_pos)
-
+        test_board = board.dup
+        test_board.move_piece(pos, end_pos)
+        test_board.in_check?(color)
     end
 end
